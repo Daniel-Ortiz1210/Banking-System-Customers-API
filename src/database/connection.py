2ba57ff2b23da2dc596974dfdab36a36d64f79a2
Daniel Ortiz <thisisdanielortiz@gmail.com>
2025-02-19 01:39:00 -0600

    Add DatabaseConnection class for managing database sessions

---
+++ src/database/connection.py
@@ -0,0 +1,47 @@
+import os
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+
+from src.utils.config import Config
+
+settings = Config()
+
+class DatabaseConnection:
+    """
+    DatabaseConnection class to manage the creation of database sessions.
+
+    Attributes:
+        _session_maker (sessionmaker): A SQLAlchemy sessionmaker instance for creating database sessions.
+
+    Methods:
+        __new__(cls):
+            Creates a new sessionmaker instance if it doesn't exist and returns a new session.
+    """
+    _session_maker: sessionmaker = None
+
+    def __new__(cls):
+        if cls._session_maker is None:
+            database_url = f"""
+            mysql+mysqlconnector://
+                {settings.database_user}:
+                {settings.database_password}@
+                {settings.database_host}:
+                {settings.database_port}/
+                {settings.database_name}
+            """
+            engine = create_engine(database_url)
+            cls._session_maker = sessionmaker(engine, autoflush=False)
+        return cls._session_maker()
+
+Base = declarative_base()
+
+def get_database_connection() -> Session:
+    db = DatabaseConnection()
+    try:
+        yield db
+    finally:
+        db.close()
+    
