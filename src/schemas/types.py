85417dea991016462f57e182bdeef0ccb011e1f5
Daniel Ortiz <thisisdanielortiz@gmail.com>
2025-02-28 11:24:40 -0600

    Add PasswordStr type with validation to schemas for enhanced security

--- src/schemas/types.py
+++ src/schemas/types.py
@@ -1,10 +1,12 @@
 from pydantic import AfterValidator
 from typing import Annotated
 
-from src.schemas.validators import validate_only_letters, validate_email, validate_phone_number
+from src.schemas.validators import validate_only_letters, validate_email, validate_phone_number, validate_password
 
 AlphaStr = Annotated[str, AfterValidator(validate_only_letters)]
 
 EmailStr = Annotated[str, AfterValidator(validate_email)]
 
 PhoneNumberStr = Annotated[str, AfterValidator(validate_phone_number)]
+
+PasswordStr = Annotated[str, AfterValidator(validate_password)]
