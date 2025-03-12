fee78a9118f7a08227f055cc985d38af59293e05
Daniel Ortiz <thisisdanielortiz@gmail.com>
2025-03-12 17:02:30 -0600

    Rename customers router to users router and include services router in API versioning

--- src/api/router.py
+++ src/api/router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
-from src.api.customers import router as customers_router
+from src.api.customers import router as users_router
 from src.api.auth import router as auth_router
+from src.api.services import router as services_router
 
 version_router = APIRouter(prefix='/api/v1')
-version_router.include_router(customers_router)
+version_router.include_router(users_router)
 version_router.include_router(auth_router)
+version_router.include_router(services_router)
